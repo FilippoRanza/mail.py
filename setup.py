37d5b65d3b7e53600aa8967a911e16aace14c7e2
Filippo Ranza <filipporanza@gmail.com>
2019-07-29 19:12:43 +0200

    added install script

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+#! /usr/bin/python3
+
+from distutils.core import setup
+
+setup(name='mail.py',
+      version='0.1',
+      description='simple cli email sender',
+      author='Filippo Ranza',
+      author_email='filipporanza@gmail.com',
+      url='https://github.com/FilippoRanza/mail.py',
+      scripts=['mail.py']
+     )
