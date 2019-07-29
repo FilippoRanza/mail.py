a47c9495a1d34fb9cfd43e15841fb9e6754cc223
Filippo Ranza <filipporanza@gmail.com>
2019-07-30 00:49:52 +0200

    improved test_mail.py

--- test_mail.py
+++ test_mail.py
@@ -4,20 +4,32 @@
 test for mail.py
 """
 
+import unittest
 from mail import load_destination
 
 
-def test_load_destination():
+class TestLoadDestination(unittest.TestCase):
     """
     test load_destination
-    """
-    correct = ['address@email.com',
-               'example@test.com',
-               'test@example.com',
-               'mail@service.com',
-               'user@mail.com',
-               'mail@mail.com']
-    # this value is a list when used in mail.py
-    ans = load_destination(['address@email.com', 'addresses_example.txt', 'mail@mail.com'])
+    """    
 
-    assert correct == ans
+    def test_load_destination(self):
+        """
+        test load_destination
+        """
+        # this list is like the one coming from arg parser
+        arg = ['address@email.com', 'addresses_example.txt', 'mail@mail.com']
+        correct = ['address@email.com',
+                   'example@test.com',
+                   'test@example.com',
+                   'mail@service.com',
+                   'user@mail.com',
+                   'mail@mail.com']
+        # this value is a list when used in mail.py
+        ans = load_destination(arg)
+
+        self.assertEqual(correct, ans)
+
+
+if __name__ == "__main__":
+    unittest.main()
